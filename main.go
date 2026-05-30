4e86f87e949b954b256f1148906b77d2c7326feb
Vaishali ojha <vaishaliojha2006@gmail.com>
2026-05-30 23:14:09 +0530

    cd works too now

--- main.go
+++ main.go
@@ -2,25 +2,18 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 )
 
-func execinput(input string) error {
-	input = strings.TrimSuffix(input, "\n")
-	args := strings.Split(input, " ")
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	return cmd.Run()
-}
-
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("> ")
+		cwd, _ := os.Getwd()
+		fmt.Printf("%s > ", cwd)
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -30,4 +23,24 @@ func main() {
 		}
 	}
 
+}
+
+func execinput(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return errors.New("destination path required")
+		}
+
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
 }
