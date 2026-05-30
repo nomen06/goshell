c997beb81ef14ca77cb0aa2af9f29be0535c96d5
Vaishali ojha <vaishaliojha2006@gmail.com>
2026-05-30 23:22:11 +0530

    a very simple shell is done

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+//i only added fmt, istg everything else just came
 import (
 	"bufio"
 	"errors"
@@ -10,13 +11,17 @@ import (
 )
 
 func main() {
+
 	reader := bufio.NewReader(os.Stdin)
+	//taking input
 	for {
 		cwd, _ := os.Getwd()
 		fmt.Printf("%s > ", cwd)
+		//new line par break karing
 		input, err := reader.ReadString('\n')
-		if err != nil {
+		if err != nil { //error
 			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
 		if err = execinput(input); err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -27,8 +32,12 @@ func main() {
 
 func execinput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
-	args := strings.Split(input, " ")
+	//earlier i had strings.Split...but this is better because of reasons like multiple spaces
+	args := strings.Fields(input)
 
+	if len(args) == 0 { //no input
+		return nil
+	}
 	switch args[0] {
 	case "cd":
 		if len(args) < 2 {
@@ -42,5 +51,6 @@ func execinput(input string) error {
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
 	return cmd.Run()
 }
